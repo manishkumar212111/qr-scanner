7308d6c8dc350134da1fe52a28c54181f153b95c
manishkumar212111 <manish.Kumar212111@gmail.com>
2024-02-21 10:21:15 +0530

    Update config.js

--- shop/src/API/config.js
+++ shop/src/API/config.js
@@ -1,7 +1,7 @@
 import { API_BASE_URL } from "../config";
 
 const IMAGE_URL = "https://ik.imagekit.io/i2wp0fsg8dx/";
-const BASE_URL = 'https://restaurant-1e1l.onrender.com' || 'http://localhost:5000/';
+const BASE_URL = 'https://restaurant-1e1l.onrender.com/' || 'http://localhost:5000/';
 // const BASE_URL = 'http://localhost:5000/';
 
 
